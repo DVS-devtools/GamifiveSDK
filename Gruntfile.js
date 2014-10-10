7fba2678552b3ee821962fcdf2cb99bfbfa510f2
Bolza <stefano.sergio@gmail.com>
2014-10-10 13:03:22 +0200

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,78 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+        // Metadata.
+    meta: {
+      version: '0.1'
+    },
+    filename: 'gfsdk',
+    buildFilename: 'dist/<%= filename %>-<%= meta.version %>.js',
+    minFilename: 'dist/<%= filename %>-<%= meta.version %>.min.js',
+    docPath: 'manual/',
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! GAMIFIVE SDK - v<%= meta.version %> - ' +
+      '<%= grunt.template.today("dd-mm-yyyy") %>\n' +
+      '* http://gamifive.com/\n' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> ' +
+      'Gamifive; Licensed MIT */\n',
+    shell: {
+      jsdoc: {
+          command: './node_modules/jsdoc/jsdoc.js ./src/gfsdk.js -d <%= docPath %>'
+      },
+      index: {  
+          command: 'rm <%= docPath %>index.html'
+      }
+    },
+    wrap: {
+      basic: {
+        src: ['<%= buildFilename %>'],
+        dest: '<%= buildFilename %>',
+        options: {
+          wrapper: ['(function(target) {\n', '\n})(window);']
+        }
+      }
+    },
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      dist: {
+        src: '<%= buildFilename %>',
+        dest: '<%= minFilename %>'
+      }
+    },
+    concat: {
+      dist: {
+        src: ['src/FB.js','src/gfsdk.js'],
+        dest: '<%= buildFilename %>'
+      }
+    },
+    jshint: {
+      options: { curly: true, eqeqeq: true, immed: true, latedef: true, newcap: true, noarg: true, sub: true, undef: true, unused: true, boss: true, eqnull: true,
+        globals: {}
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib_test: {
+        src: ['lib/**/*.js', 'test/**/*.js']
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-wrap');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  //grunt.loadNpmTasks('grunt-jsdoc');
+
+  // Default task.
+  grunt.registerTask('build', ['shell:jsdoc', 'shell:index','concat', 'wrap', 'uglify']);
+
+
+};
---
+++ src/Leaderboard.js
@@ -0,0 +1,17 @@
+var getUser = function() {
+	return {
+		name: 'Gigi',
+		leaderboard: [
+			{member: "29d7b4600ddd11e48863005056b60718",score: 25372,rank: 1,avatar: "avatar_018.png",nickname: "Francesco",me: false},
+			{member: "617711c610a911e4bd78005056b60716",score: 14491,rank: 2,avatar: "avatar_020.png",nickname: "nipac",me: false},
+			{member: "6eb368aa0dc311e4a405005056b60718",score: 13967,rank: 3,avatar: "avatar_016.png",nickname: "LouCarrot",me: false},
+			{member: "c3927dd849ce11e4b11a005056b60718",score: 10858,rank: 4,avatar: "avatar_016.png",nickname: "player_542ca2",me: false},
+			{member: "57390c78342111e495be005056b60718",score: 7227,rank: 5,avatar: "avatar_010.png",nickname: "Massimo",me: false},
+			{member: "a2ba854c335d11e4a47f005056b60718",score: 7120,rank: 6,avatar: "avatar_00.png",nickname: "Matteo",me: false}
+		]
+	}
+}
+
+var renderLeaderboard = function () {
+	
+}
