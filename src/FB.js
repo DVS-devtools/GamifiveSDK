555f93780ce7ae9c931a47dd531bf26136feba04
Bolza <stefano.sergio@gmail.com>
2014-10-13 16:43:39 +0200

    safari links fix

--- src/FB.js
+++ src/FB.js
@@ -121,3 +121,13 @@
 			console.log(response);
 		});
 	}
+	
+	var a=document.getElementsByTagName("a");
+	for(var i=0;i<a.length;i++)
+	{
+	    a[i].onclick=function()
+	    {
+	        window.location=this.getAttribute("href");
+	        return false
+	    }
+	}
