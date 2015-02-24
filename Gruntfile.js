6c2587840813cff5dd48187a45c1578723d513fe
aureliome <aurelio.me@gmail.com>
2015-02-24 16:17:14 +0100

    New environment

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,99 +1,149 @@
 /*global module:false*/
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    // version
-    version: '0.3',
+	// Project configuration.
+	grunt.initConfig({
+		// version
+		version: '0.4',
 
-    // paths
-    docPath: 'jsdoc/',
-    srcPath: 'src/',
-    distPath: 'dist/',
+		// paths
+		docPath: 'jsdoc/',
+		srcPath: 'src/',
+		distPath: 'dist/',
+		tmpPath: '.tmp/',
+		demoPath: 'demo/',
 
-    filename: 'gfsdk',
-    maxFilename: '<%= distPath %><%= filename %>-<%= version %>.js',
-    minFilename: '<%= distPath %><%= filename %>-<%= version %>.min.js',
-    
-    // pkg: grunt.file.readJSON('package.json'),
-    banner: '/*! GAMIFIVE SDK - v<%= version %> - ' +
-      '<%= grunt.template.today("dd-mm-yyyy") %>\n' +
-      '* http://gamifive.com/\n' +
-      '* Copyright (c) <%= grunt.template.today("yyyy") %> ' +
-      'Gamifive; Licensed MIT */\n',
-    
-    shell: {
-      index: {  
-          command: 'cp <%= docPath %>GamefiveSDK.html <%= docPath %>index.html'
-      }
-    },
-    jsdoc : {
-        dist : {
-            src: ['<%= srcPath %>gfsdk.js'], 
-            options: {
-                destination: 'jsdoc'
-            }
-        }
-    },
-    qunit: {
-      all: {
-        options: {
-          urls: ['http://localhost:8000/test/all.html']
-        }
-      }
-    },
-    wrap: {
-      basic: {
-        src: ['<%= maxFilename %>'],
-        dest: '<%= maxFilename %>',
-        options: {
-          wrapper: ['(function(target) {\n', '\n target.GamefiveSDK = new GamefiveSDK(); \n})(window);']
-        }
-      }
-    },
-    uglify: {
-      options: {
-        banner: '<%= banner %>',
-        sourceMap: true
-      },
-      dist: {
-        src: '<%= maxFilename %>',
-        dest: '<%= minFilename %>'
-      }
-    },
-    concat: {
-      dist: {
-        src: ['<%= srcPath %>*'],
-        dest: '<%= maxFilename %>'
-      }
-    },
-    jshint: {
-      options: { curly: true, eqeqeq: true, immed: true, latedef: true, newcap: true, noarg: true, sub: true, undef: true, unused: true, boss: true, eqnull: true,
-        globals: {}
-      },
-      gruntfile: {
-        src: 'Gruntfile.js'
-      },
-    },
-    clean: {
-      jsdoc: ['<%= docPath %>'],
-      dist: ['<%= distPath %>']
-    }
-  });
+		// filenames
+		filename: 'gfsdk',
+		maxFilename: '<%= distPath %><%= filename %>-<%= version %>.js',
+		minFilename: '<%= distPath %><%= filename %>-<%= version %>.min.js',
+		
+		// banner
+		banner: '/*! GAMIFIVE SDK - v<%= version %> - ' +
+			'<%= grunt.template.today("dd-mm-yyyy") %>\n' +
+			'* http://gamifive.com/\n' +
+			'* Copyright (c) <%= grunt.template.today("yyyy") %> ' +
+			'Gamifive; Licensed MIT */\n',
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-wrap');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-shell');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
-  grunt.loadNpmTasks('grunt-jsdoc');
-  grunt.loadNpmTasks('grunt-contrib-clean');
+		// tasks config
+		clean: {
+			doc: ['<%= docPath %>'],
+			dist: ['<%= distPath %>'],
+			tmp: ['<%= tmpPath %>']
+		},
 
-  // Default task.
-  grunt.registerTask('build', ['clean','jsdoc','shell:index','concat', 'wrap', 'uglify']);
-  grunt.registerTask('test', ['concat', 'wrap', 'qunit']);
+		connect: {
+			options: {
+				port: 9000,
+				hostname: 'localhost',
+				livereload: 35729
+			},
+			server: {
+				options: {
+					open: 'http://localhost:9000/<%= demoPath %>',
+					base: [
+						'.'
+					]
+				}
+			}
+		},
+
+		watch: {
+			livereload: {
+				files: [
+					'<%= srcPath %>/**/*',
+					'<%= demoPath %>/**/*'
+				],
+				options: {
+					livereload: '<%= connect.options.livereload %>'
+				}				
+			}
+		},
+
+		jsdoc : {
+			dist : {
+				src: ['<%= srcPath %>*'], 
+					options: {
+						destination: '<%= docPath %>'
+				}
+			}
+		},
+		
+		// shell: {
+		// 	index: {	
+		// 		command: 'cp <%= docPath %>GamefiveSDK.html <%= docPath %>index.html'
+		// 	}
+		// },
+
+		concat: {
+			dist: {
+				src: ['<%= srcPath %>*'],
+				dest: '<%= maxFilename %>'
+			}
+		},
+		
+		wrap: {
+			basic: {
+				src: ['<%= maxFilename %>'],
+				dest: '<%= maxFilename %>',
+				options: {
+					wrapper: ['(function(target) {\n', '\n target.GamefiveSDK = new GamefiveSDK(); \n})(window);']
+				}
+			}
+		},
+
+		uglify: {
+			options: {
+				banner: '<%= banner %>',
+				sourceMap: true
+			},
+			dist: {
+				src: '<%= maxFilename %>',
+				dest: '<%= minFilename %>'
+			}
+		},
+
+		qunit: {
+			all: {
+				options: {
+					urls: ['http://localhost:8000/test/all.html']
+				}
+			}
+		}
+	});
+
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-qunit');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-jsdoc');
+	grunt.loadNpmTasks('grunt-shell');	
+	grunt.loadNpmTasks('grunt-wrap');	
+
+	// Tasks
+	grunt.registerTask('serve', [
+		'connect:server',
+		'watch'
+	]);
+
+	grunt.registerTask('build', [
+		'clean:dist',
+		'clean:doc',
+		'jsdoc',
+		// 'shell:index',
+		'concat', 
+		'wrap', 
+		'uglify'
+	]);
+
+	grunt.registerTask('test', [
+		'concat', 
+		'wrap', 
+		'qunit'
+	]);
 
 
 };
