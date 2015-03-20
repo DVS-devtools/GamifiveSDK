35e1154d5b721d8f9a358730c8d09ccbce06c586
ffognani <fabio.fognani@gmail.com>
2015-03-20 12:22:08 +0100

    Testing init method

---
+++ test/karma/mock/APIMockResponse.js
@@ -0,0 +1,32 @@
+var APIConfigMockResponse = {
+    "label": "it_igames",
+    "contentId": "ad4e47670365ff61cc392d3201f53a0f",
+    "userId": "8a81885285e411e49069005056b60712",
+    "fbUserId": null,
+    "fbAppId": "497938953670292",
+    "fbConnected": true,
+    "requireFbConnect": true,
+    "fbExternal": false,
+    "userFreemium": false,
+    "challenge": {
+        "id": null
+    },
+    "dictionary": {
+        "messageOfFbChallenge": "My score is %s, try to beat me!",
+        "matchLeftSingular": "you have <span>%s</span> match left",
+        "matchLeftPlural": "you have <span>%s</span> matches left",
+        "matchLeftNone": "You don't have any credits left"
+    },
+    "user": {
+        "userId": "8a81885285e411e49069005056b60712",
+        "fbUserId": null,
+        "fbConnected": true,
+        "userFreemium": false,
+        "nickname": "Ezechiele",
+        "avatar": {
+            "src": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAPoAAAD6CAMAAAC/MqoPAAACHFBMV…WFhYWFhYWFhYWFhYWFhYWFhYWFxZ+NglEwCkbBKBgFwwcAAJmyY+uMlOW9AAAAAElFTkSuQmCC",
+            "name": "avatar_08.png"
+        }
+    },
+    "success": true
+};
--- test/karma/spec/test.js
+++ test/karma/spec/test.js
@@ -3,6 +3,14 @@
 (function () {
     describe('GamifiveSDK', function () {
 
+    	var $httpBackend;
+ 
+		beforeEach(inject(function(_$httpBackend_) {
+		    $httpBackend = _$httpBackend_;
+		    $httpBackend.when('GET', '*/gamifiveinfo')
+		      .respond(APIConfigMockResponse);
+		}));
+
     	afterEach(function() {
     		// reset the configuration (this is the only way to reset the status of the module)
 		  	GamifiveSDK.reset();
@@ -72,7 +80,7 @@
 	      		});
 	      	});
 
-	      	it('if debug is false, then config == window.GamifiveInfo', function(){
+	      	it('if debug is false, then config is taken from window.GamifiveInfo', function(){
 	      		var param = { 
 	      			debug: false
 	      		};
@@ -93,6 +101,68 @@
 	      			for (var key in window.GamifiveInfo){
 		      			expect(config[key]).toEqual(window.GamifiveInfo[key]);
 		      		}
+	      		});
+	      	});
+
+	      	it('if debug is true, then config is taken from API', function(){
+	      		
+	      	});
+
+	      	it('if lite is true, FBConnector is not initialized and GameOver listeners are not added', function(){
+	      		var param = { 
+	      			lite: true
+	      		};
+
+	      		runs(function(){
+	      			spyOn(FBConnector, 'setConfig');
+	      			spyOn(FBConnector, 'start');
+	      			spyOn(GameOverCore, 'addListeners');
+	      			spyOn(GamifiveSDK, 'controlFbConnected');
+	      			GamifiveSDK.init(param);
+	      		});
+	      		
+	      		var config; 
+
+	      		waitsFor(function() {
+	      			// Wait for api to return value
+					config = GamifiveSDK.getConfig();
+					return !!config && !!config.contentId;
+			    }, 'wait contentId', 3000);
+
+	      		runs(function(){
+	      			expect(FBConnector.setConfig).not.toHaveBeenCalled();
+	      			expect(FBConnector.start).not.toHaveBeenCalled();
+	      			expect(GameOverCore.addListeners).not.toHaveBeenCalled();
+					expect(GamefiveSDK.controlFbConnected).not.toHaveBeenCalled();
+	      		});
+	      	});
+
+	      	it('if lite is false, FBConnector is initialized and GameOver listeners are added', function(){
+	      		var param = { 
+	      			lite: false
+	      		};
+
+	      		runs(function(){
+	      			spyOn(FBConnector, 'setConfig');
+	      			spyOn(FBConnector, 'start');
+	      			spyOn(GameOverCore, 'addListeners');
+	      			spyOn(GamifiveSDK, 'controlFbConnected');
+	      			GamifiveSDK.init(param);
+	      		});
+	      		
+	      		var config; 
+
+	      		waitsFor(function() {
+	      			// Wait for api to return value
+					config = GamifiveSDK.getConfig();
+					return !!config && !!config.contentId;
+			    }, 'wait contentId', 3000);
+
+	      		runs(function(){
+	      			expect(FBConnector.setConfig).toHaveBeenCalled();
+	      			expect(FBConnector.start).toHaveBeenCalled();
+	      			expect(GameOverCore.addListeners).toHaveBeenCalled();
+					expect(GamefiveSDK.controlFbConnected).toHaveBeenCalled();
 	      		});
 	      	});
 	      	
