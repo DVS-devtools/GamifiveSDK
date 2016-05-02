a954bdb952f7ee293de057f07da873e6e43e816c
ffognani <fabio.fognani@buongiorno.com>
2016-05-02 15:26:25 +0200

    added check (gen/vhost exists) before creating generated file

--- compile_vhost.sh
+++ compile_vhost.sh
@@ -1,3 +1,8 @@
+#!/bin/sh
+
+# create directory if not exists
+[ -d gen/vhost ] || mkdir gen/vhost -p
+
 grep -Porih "(?<=VHost.get\(')[^'\)]*" src | # get VHost.get('...') occurrencies in all the code
 sort -u |   # sort them and remove duplicates
 awk ' BEGIN { ORS = "\n"; print "//WARNING: THIS FILE IS AUTO-GENERATED, DO NOT MODIFY IT (use npm run vhost:keys to rebuild it)\nmodule.exports = ["; } { print "\t/@"$0"/@,"; } END { print "];"; }' | # format them as JSON
