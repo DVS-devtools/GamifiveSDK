37eda96d651ddc5e23649c8462a5fb7acbf2f80a
ffognani <fabio.fognani@buongiorno.com>
2016-05-02 17:22:54 +0200

    added double quotes in grep for vhost keys

--- compile_vhost.sh
+++ compile_vhost.sh
@@ -3,7 +3,7 @@
 # create directory if not exists
 [ -d gen/vhost ] || mkdir gen/vhost -p
 
-grep -Porih "(?<=VHost.get\(')[^'\)]*" src | # get VHost.get('...') occurrencies in all the code
-sort -u |   # sort them and remove duplicates
+grep -Porih "(?<=VHost.get\(('|\"))[^('|\")\)]*" src | # get VHost.get('...') occurrencies in all the code
+sort -u | # sort them and remove duplicates
 awk ' BEGIN { ORS = "\n"; print "//WARNING: THIS FILE IS AUTO-GENERATED, DO NOT MODIFY IT (use npm run vhost:keys to rebuild it)\nmodule.exports = ["; } { print "\t/@"$0"/@,"; } END { print "];"; }' | # format them as JSON
 sed "s^\"^\\\\\"^g;s^\/\@\/\@^\", \"^g;s^\/\@^\"^g" > gen/vhost/vhost-keys.js
