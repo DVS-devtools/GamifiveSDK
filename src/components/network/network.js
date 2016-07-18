eadcac3995b168c6f9f252297428fb7756d8659d
pasquale <pasqualemangialavori@buongiorno.com>
2016-07-18 12:20:47 +0200

    network: xhr now works also with promise interface

--- src/components/network/network.js
+++ src/components/network/network.js
@@ -19,21 +19,21 @@ var Network = new function(){
         Logger.log('GamifiveSDK', 'Network', 'xhr', method, url);
         
         var xhr = new XMLHttpRequest();
-        
-        xhr.onreadystatechange = function(){
-            if ( xhr.readyState === 4 ) {
-                var resp = xhr;
-                resp.success = (xhr.status >= 200 && xhr.status <= 399);
-                if (callback) {
-                    callback(resp);
+        return new Promise(function(resolve, reject){
+            xhr.onreadystatechange = function(){
+                if ( xhr.readyState === 4 ) {
+                    var resp = xhr;
+                    resp.success = (xhr.status >= 200 && xhr.status <= 399);
+                    if (callback) {
+                        callback(resp);
+                    }
+                    resolve(resp);
                 }
-            }
-        };
+            };
 
-        xhr.open(method, url);
-        xhr.send();
-
-        return xhr;
+            xhr.open(method, url);
+            xhr.send();
+        });
     }
 
 };
