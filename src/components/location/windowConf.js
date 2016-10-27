1cbdfb11550d45b16a6260bf9a099b95ce3b85f0
pasquale <pasqualemangialavori@buongiorno.com>
2016-10-27 14:17:24 +0200

    default gameid

--- src/components/location/windowConf.js
+++ src/components/location/windowConf.js
@@ -1,4 +1,4 @@
-module.exports = function(HOST, GAME_ID, COUNTRY_CODE = "ww-it"){
+module.exports = function(HOST, GAME_ID = 'fakeid', COUNTRY_CODE = "ww-it"){
     return {
         "hash": "",
         "search": "",
