2495e3153b73aab3f257c4bb9939cf832525a90e
pasquale <pasqualemangialavori@buongiorno.com>
2016-12-20 12:07:41 +0100

    Added country and real country to newton login

--- src/components/session/session.js
+++ src/components/session/session.js
@@ -267,18 +267,30 @@ var Session = new function(){
                         }
                     });
 
-                    var queryString = Location.getQueryString();
+                    let queryString = Location.getQueryString();
                     if (getType(queryString.dest) === 'undefined'){
     					queryString.dest = 'N/A';
                     }
+                    
+                    let toAdd = [
+                        ['country', VHost.get('TLD')], 
+                        ['real_country', VHost.get('NT_REAL_COUNTRY')],
+                        ['white_label_id', GameInfo.getInfo().label],
+                        ['http_referrer', window.document.referrer]
+                    ];
 
-                    queryString.http_referrer = window.document.referrer;
-                    queryString.white_label_id = GameInfo.getInfo().label;
+                    let userProperties = toAdd.reduce((accumulator, keyValue)=>{
+                        if(keyValue[1]){
+                            accumulator[keyValue[0]] = keyValue[1];
+                        }
+                        return accumulator;
+                    }, queryString);
+
                     /** wait newton login */
                     return NewtonService.login({
                         type: 'external',
                         userId: User.getUserId(), 
-                        userProperties: queryString,
+                        userProperties: userProperties,
                         logged: (User.getUserType() !== 'guest')
                     }).catch((reason)=>{
                         return Promise.resolve();
--- src/version.js
+++ src/version.js
@@ -1,1 +1,1 @@
-var pkgInfo = {"version":"2.0.5", "build":"v2.0.5-9-g6cde043"};module.exports = pkgInfo;
+var pkgInfo = {"version":"2.0.5", "build":"v2.0.5-10-gd94954e"};module.exports = pkgInfo;
